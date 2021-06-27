9c1744c64666dda5e855668690f86c1f62971daa
Laks <lakhsmycastro336@gmail.com>
2021-06-27 18:53:19 -0300

    apply fix: https://github.com/AnimeFlitch/flitch/issues/15#issuecomment-869197783 (temporary yet, but working)

--- lib/app/core/details/external/data_source/anime_tv_details_data_source.dart
+++ lib/app/core/details/external/data_source/anime_tv_details_data_source.dart
@@ -1,19 +1,100 @@
-import 'package:anime_dart/app/constants/utils.dart';
+import 'dart:io';
+import 'dart:math' as math;
+import 'dart:typed_data';
 
+import 'package:anime_dart/app/constants/utils.dart';
 import 'package:anime_dart/app/core/details/infra/data_source/details_data_source.dart';
 import 'package:anime_dart/app/core/details/infra/models/anime_details_model.dart';
 import 'package:anime_dart/app/core/details/infra/models/episode_details_model.dart';
 import 'package:anime_dart/app/core/favorites/domain/repositories/favorite_repository.dart';
 import 'package:anime_dart/app/core/watched/domain/repository/watched_repository.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+
+/// Ok, I know, it's a dirty code, the worst way to code, the global
+/// variables, but I have no time for the best practices here
+/// Sorry for this, someday I fix this.
+String _tempToken;
+int _count;
 
 class AnimeTvDetailsDataSource implements DetailsDataSource {
   final _baseUrl = "https://appanimeplus.tk/meuanimetv-40.php";
   final _imageBaseUrl = "https://cdn.appanimeplus.tk/img/";
 
+  static Future<File> writeToFile(ByteData data) async {
+    final buffer = data.buffer;
+    Directory tempDir = await getTemporaryDirectory();
+    String tempPath = tempDir.path;
+
+    var filePath =
+        tempPath + '/file_01.dat'; // file_01.tmp is dump file, can be anything
+
+    return File(filePath).writeAsBytes(
+      buffer.asUint8List(
+        data.offsetInBytes,
+        data.lengthInBytes,
+      ),
+    );
+  }
+
+  static Future<Map<String, String>> get authHeaders async {
+    if (_tempToken == null || _count >= 40) {
+      final authFileData = await rootBundle
+          .load('lib/app/core/details/external/data_source/file.dat');
+
+      final authList = authFileData.buffer.asUint8List();
+
+      final authFile = await writeToFile(authFileData);
+
+      final length = authFile.lengthSync();
+
+      final xAuth = await Dio(
+        BaseOptions(
+          baseUrl: 'https://auth.appanimeplus.tk',
+          headers: <String, String>{
+            'Host': 'auth.appanimeplus.tk',
+            'user-agent': Utils.randomUserAgent,
+            'content-type': 'application/octet-stream',
+            'accept': '*/*',
+            'content-length': '$length',
+          },
+        ),
+      ).post(
+        '',
+        data: Stream<List<int>>.fromIterable(authList.map((e) => [e])),
+      );
+
+      _count = 0;
+
+      _tempToken = xAuth.data;
+    } else {
+      _count++;
+    }
+
+    if (_tempToken == null) return <String, String>{};
+
+    return <String, String>{
+      "X-Auth": _tempToken,
+      "X-Requested-With": "br.com.meuanimetv",
+    };
+  }
+
   final FavoritesRepository favorites;
   final WatchedRepository watched;
 
   final dio = Utils.dio;
+
+  static Map<String, String> get streamingDataR {
+    final r = (math.Random().nextDouble() * 90000).floor() + 10000;
+    final time = (DateTime.now().millisecondsSinceEpoch / 1000) * 2;
+    final token = (time * r).round();
+
+    return {
+      'r': '$r',
+      'token': '$token',
+    };
+  }
 
   AnimeTvDetailsDataSource({
     this.favorites,
@@ -61,7 +142,18 @@ class AnimeTvDetailsDataSource implements DetailsDataSource {
   @override
   Future<EpisodeDetailsModel> getEpisodeDetails(String id) async {
     try {
-      final response = await dio.get(_baseUrl + "?episodios=$id");
+      final response = await dio.get(
+        _baseUrl,
+        queryParameters: {
+          'episodios': id,
+          ...streamingDataR,
+        },
+        options: Options(
+          headers: <String, String>{
+            ...(await authHeaders),
+          },
+        ),
+      );
 
       final data = response.data[0];
 
@@ -83,7 +175,7 @@ class AnimeTvDetailsDataSource implements DetailsDataSource {
         "urlFullHd": data["locationhd"],
         "imageUrl": ownerAnime.imageUrl,
         "imageHttpHeaders": ownerAnime.imageHttpHeaders,
-        "stats": stats
+        "stats": stats,
       };
 
       final result = EpisodeDetailsModel(
@@ -112,8 +204,18 @@ class AnimeTvDetailsDataSource implements DetailsDataSource {
       final ownerAnime = await getAnimeDetails(currentEpisode.animeId);
 
       final response = await dio.get(
-        _baseUrl +
-            "?episodios=${currentEpisode.id}&catid=${ownerAnime.id}&next",
+        _baseUrl,
+        queryParameters: {
+          'episodios': 'currentEpisode.id',
+          'catid': ownerAnime.id,
+          'next': '',
+          ...streamingDataR,
+        },
+        options: Options(
+          headers: <String, String>{
+            ...(await authHeaders),
+          },
+        ),
       );
 
       final data = response.data[0];
@@ -131,9 +233,10 @@ class AnimeTvDetailsDataSource implements DetailsDataSource {
         "label": data["title"],
         "url": data["location"],
         "urlHd": data["locationsd"],
+        "urlFullHd": data["locationhd"],
         "imageUrl": ownerAnime.imageUrl,
         "imageHttpHeaders": ownerAnime.imageHttpHeaders,
-        "stats": stats
+        "stats": stats,
       };
 
       final nextEpisode = EpisodeDetailsModel.fromMap(source);
@@ -171,9 +274,10 @@ class AnimeTvDetailsDataSource implements DetailsDataSource {
         "label": data["title"],
         "url": data["location"],
         "urlHd": data["locationsd"],
+        "urlFullHd": data["locationhd"],
         "imageUrl": ownerAnime.imageUrl,
         "imageHttpHeaders": ownerAnime.imageHttpHeaders,
-        "stats": stats
+        "stats": stats,
       };
 
       final previousEpisode = EpisodeDetailsModel.fromMap(source);
@@ -189,7 +293,18 @@ class AnimeTvDetailsDataSource implements DetailsDataSource {
     try {
       final targetAnime = await getAnimeDetails(id);
 
-      final response = await dio.get(_baseUrl + "?cat_id=${targetAnime.id}");
+      final response = await dio.get(
+        _baseUrl,
+        queryParameters: {
+          'cat_id': targetAnime.id,
+          ...streamingDataR,
+        },
+        options: Options(
+          headers: <String, String>{
+            ...(await authHeaders),
+          },
+        ),
+      );
 
       final data = response.data;
 
@@ -209,6 +324,7 @@ class AnimeTvDetailsDataSource implements DetailsDataSource {
           "label": episode["title"],
           "url": episode["location"],
           "urlHd": episode["locationsd"],
+          "urlFullHd": data["locationhd"],
           "imageUrl": targetAnime.imageUrl,
           "imageHttpHeaders": targetAnime.imageHttpHeaders,
           "stats": stats
